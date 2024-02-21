a32ad56a2031b4563bae868259e7cc4930af8cc6
elizaveta.andreeva <elizaveta.andreeva@corp.mail.ru>
2024-02-22 00:50:38 +0300

    ФП 10 fx

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -53,6 +53,9 @@ public class InMemoryFilmStorage implements FilmStorage {
 
     @Override
     public Film getFilmById(int id) {
+        if (id < 0) {
+            throw new ValidationException("Отрицательный id");
+        }
         if (!films.containsKey(id)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Не нашли фильм по id");
         }
