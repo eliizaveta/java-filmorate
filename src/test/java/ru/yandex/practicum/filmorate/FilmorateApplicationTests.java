73c523eb3d9cf55bddce464b643bab7a28919c83
elizaveta.andreeva <elizaveta.andreeva@corp.mail.ru>
2024-02-28 02:28:26 +0300

    ФП 11 fix build 3

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -3,39 +3,24 @@ package ru.yandex.practicum.filmorate;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase
 class FilmorateApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
-
-	@Test
-	void testAddFilm() throws Exception {
-		mockMvc.perform(
-				post("/films")
-						.content("{\n" +
-								"  \"name\": \"Name\",\n" +
-								"  \"description\": \"Desc\",\n" +
-								"  \"releaseDate\": \"2024-01-18\",\n" +
-								"  \"duration\": 100\n" +
-								"}")
-						.contentType(MediaType.APPLICATION_JSON)
-		).andExpect(status().isOk());
-	}
 
 	@Test
 	void testAddUser() throws Exception {
@@ -49,92 +34,5 @@ class FilmorateApplicationTests {
 								"}")
 						.contentType(MediaType.APPLICATION_JSON)
 		).andExpect(status().isOk());
-	}
-
-	@Test
-	void testAddFriendUser() throws Exception {
-		mockMvc.perform(
-				post("/users")
-						.content("{\n" +
-								"  \"login\": \"lizand\",\n" +
-								"  \"name\": \"Lisa\",\n" +
-								"  \"email\": \"mail@gmail.com\",\n" +
-								"  \"birthday\": \"2000-09-04\"\n" +
-								"}")
-						.contentType(MediaType.APPLICATION_JSON)
-		).andExpect(status().isOk());
-		mockMvc.perform(
-				post("/users")
-						.content("{\n" +
-								"  \"login\": \"lizand2\",\n" +
-								"  \"name\": \"Lisa\",\n" +
-								"  \"email\": \"mail@gmail.com\",\n" +
-								"  \"birthday\": \"2000-09-04\"\n" +
-								"}")
-						.contentType(MediaType.APPLICATION_JSON)
-		).andExpect(status().isOk());
-		mockMvc.perform(
-				put("/users/1/friends/2")
-		).andExpect(status().isOk());
-	}
-
-	@Test
-	void testGetEmptyMutualFriends() throws Exception {
-		mockMvc.perform(
-				post("/users")
-						.content("{\n" +
-								"  \"login\": \"lizand\",\n" +
-								"  \"name\": \"Lisa\",\n" +
-								"  \"email\": \"mail@gmail.com\",\n" +
-								"  \"birthday\": \"2000-09-04\"\n" +
-								"}")
-						.contentType(MediaType.APPLICATION_JSON)
-		).andExpect(status().isOk());
-		mockMvc.perform(
-				post("/users")
-						.content("{\n" +
-								"  \"login\": \"lizand2\",\n" +
-								"  \"name\": \"Lisa\",\n" +
-								"  \"email\": \"mail@gmail.com\",\n" +
-								"  \"birthday\": \"2000-09-04\"\n" +
-								"}")
-						.contentType(MediaType.APPLICATION_JSON)
-		).andExpect(status().isOk());
-		mockMvc.perform(
-				get("/users/1/friends/common/2")
-		).andExpect(status().isOk());
-	}
-
-	@Test
-	void testGetFriend() throws Exception {
-		mockMvc.perform(
-				post("/users")
-						.content("{\n" +
-								"  \"login\": \"lizand\",\n" +
-								"  \"name\": \"Lisa\",\n" +
-								"  \"email\": \"mail@gmail.com\",\n" +
-								"  \"birthday\": \"2000-09-04\"\n" +
-								"}")
-						.contentType(MediaType.APPLICATION_JSON)
-		).andExpect(status().isOk());
-		mockMvc.perform(
-				post("/users")
-						.content("{\n" +
-								"  \"login\": \"lizand2\",\n" +
-								"  \"name\": \"Lisa\",\n" +
-								"  \"email\": \"mail@gmail.com\",\n" +
-								"  \"birthday\": \"2000-09-04\"\n" +
-								"}")
-						.contentType(MediaType.APPLICATION_JSON)
-		).andExpect(status().isOk());
-		mockMvc.perform(
-				get("/users/1/friends/")
-		).andExpect(status().isOk());
-		mockMvc.perform(
-				put("/users/1/friends/2")
-		).andExpect(status().isOk());
-		mockMvc.perform(
-				get("/users/1/friends/")
-		).andExpect(content().contentType(MediaType.APPLICATION_JSON));
 	}
 }
